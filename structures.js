740e14ac689f76adbe6a56ca25ba94dd9600453f
alerosa77 <alemdr7@gmail.com>
2024-10-30 17:21:01 -0300

    Update structures.js

--- structures.js
+++ structures.js
@@ -9,23 +9,3 @@ function placeBuilding(buildingType, tileX, tileY) {
         alert("Not enough DNA Units!");
     }
 }
-
-function deductCost(buildingType) {
-    const buildingCosts = {
-        "Power Plant": 30,
-        "Extractor": 20,
-        "Pylon": 10,
-        "Laser Tower": 40,
-    };
-    dnaUnits -= buildingCosts[buildingType]; // Deduct cost from DNA Units
-}
-
-function canAffordBuilding(buildingType) {
-    const buildingCosts = {
-        "Power Plant": 30,
-        "Extractor": 20,
-        "Pylon": 10,
-        "Laser Tower": 40,
-    };
-    return dnaUnits >= buildingCosts[buildingType];
-}
